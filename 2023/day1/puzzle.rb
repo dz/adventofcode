5c80ca21786e32437cbb32d2ab28470800f8004a
dz <david@nodnod.net>
2023-12-01 14:58:58 -0500

    tighten up

--- 2023/day1/puzzle.rb
+++ 2023/day1/puzzle.rb
@@ -2,8 +2,7 @@
 sum = 0
 File.readlines('input.txt').each do |line|
   d1, d2 = line.match(/\D*(\d)?.*(\d)\D*/i).captures
-  d1 = d2 if d1.nil?
-  sum = sum + (d1.to_s + d2.to_s).to_i
+  sum = sum + ((d1 || d2).to_s + d2.to_s).to_i
 end
 
 puts sum
