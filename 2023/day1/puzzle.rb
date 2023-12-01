1e7e5a726f3928e5dc40e359e9439ad3b531a5bf
dz <david@nodnod.net>
2023-12-01 14:59:34 -0500

    tighten up

--- 2023/day1/puzzle.rb
+++ 2023/day1/puzzle.rb
@@ -2,7 +2,7 @@
 sum = 0
 File.readlines('input.txt').each do |line|
   d1, d2 = line.match(/\D*(\d)?.*(\d)\D*/i).captures
-  sum = sum + ((d1 || d2).to_s + d2.to_s).to_i
+  sum += ((d1 || d2).to_s + d2.to_s).to_i
 end
 
 puts sum
