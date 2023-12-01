6e6bb09c6c92436293e95a5e2588563396d860df
dz <david@nodnod.net>
2023-12-01 17:50:28 -0500

    puzzle 2

--- 2023/day1/puzzle2.rb
+++ 2023/day1/puzzle2.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
 sum = 0
+
+word_map = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+
 File.readlines('input.txt').each do |line|
   d1, d2 = line.match(/\D*(\d)?.*(\d)\D*/i).captures
   sum += ((d1 || d2).to_s + d2.to_s).to_i
 end
-puts "digits only: #{sum}"
+puts "digits+words: #{sum}"
